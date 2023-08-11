1d289a45ffa952cd822d41e811c64668acbeefa1
Scynes <dustin.greyfield@gmail.com>
2023-08-11 12:47:03 -0400

    chore(init): added react-hook-form/zod/bootstrap dependencies. added vite aliases for components/assets. deleted default scaffolding.

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@components': '/src/components',
+            '@assets': '/src/assets'
+        },
+    },
 })
