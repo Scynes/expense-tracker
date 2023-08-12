b373b3149bc9331f52ab8b7be274d18d28e38f2d
Scynes <dustin.greyfield@gmail.com>
2023-08-12 11:30:21 -0400

    chore(ExpenseTable): added expense interface and minor changes to the ExpenseTable component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import '@assets/index.css';
+
+import { useState } from 'react';
+
 import ExpenseForm from '@components/ExpenseForm';
 import ExpenseTable from '@components/ExpenseTable';
 
+interface ExpenseItem {
+    date: string,
+    description: string,
+    category: string,
+    amount: number
+}
+
 const App = () => {
+
+    const [ expenseList, setExpenseList ] = useState<ExpenseItem[]>([]);
+
     return (
         <>
         <ExpenseForm />
--- src/components/ExpenseTable/ExpenseTable.tsx
+++ src/components/ExpenseTable/ExpenseTable.tsx
@@ -1,7 +1,16 @@
 const ExpenseTable = () => {
+
     return (
-        <>
-        </>
+        <table className="table">
+            <thead className="table-dark">
+                <tr>
+                    <th>Date</th>
+                    <th>Description</th>
+                    <th>Category</th>
+                    <th>Amount</th>
+                </tr>
+            </thead>
+        </table>
     );
 }
 
