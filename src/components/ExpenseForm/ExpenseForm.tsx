dcb6234f377f135c35fc6ae3f045f8484719c29d
Scynes <dustin.greyfield@gmail.com>
2023-08-12 10:27:05 -0400

    chore(ExpenseForm): added form input validation, temp alert.

--- src/components/ExpenseForm/ExpenseForm.tsx
+++ src/components/ExpenseForm/ExpenseForm.tsx
@@ -2,7 +2,7 @@ import { FieldValues, useForm } from 'react-hook-form';
 
 const ExpenseForm = () => {
 
-    const { register, handleSubmit, getValues } = useForm();
+    const { register, handleSubmit, formState: { isValid } } = useForm();
 
     const categories = ['Groceries', 'Utilities', 'Entertainment'];
 
@@ -11,17 +11,17 @@ const ExpenseForm = () => {
     return (
         <form className="width-full" onSubmit={ handleSubmit(onSubmit) }>
             <div className="form-floating mb-3">
-                <input { ...register('description') } type="text" id="description" className="form-control" />
+                <input { ...register('description', { required: 'Please enter' }) } type="text" id="description" className="form-control" />
                 <label htmlFor="description">Description</label>
             </div>
             <div className="form-floating mb-3">
-                <input { ...register('amount') } type="text" id="amount" className="form-control" />
+                <input { ...register('amount', { required: true }) } type="text" id="amount" className="form-control" />
                 <label htmlFor="amount">Amount</label>
             </div>
             <div className="form-floating mb-3">
-                <select { ...register('category') } id="category" className="form-select" defaultValue={""}>
+                <select { ...register('category', { required: true }) } id="category" className="form-select" defaultValue={""}>
                     <option value="" disabled hidden />
-                    <option value="empty" disabled label="Choose A Category" />
+                    <option value="empty" disabled label="Choose a Category" />
                     { categories.map( (category, index) => (
                         <option key={ index } value={ category }>
                             { category }
@@ -30,7 +30,16 @@ const ExpenseForm = () => {
                 </select>
                 <label htmlFor="category">Category</label>
             </div>
-            <button className="btn btn-dark btn-lg w-100" type="submit">Submit</button>
+            <div className="form-floating mb-3">
+                <input { ...register('date', { required: true }) } type="date" id="date" className="form-control" />
+                <label htmlFor="date">Choose a Date</label>
+            </div>
+            <button className="mb-3 btn btn-dark btn-lg w-100" type="submit" disabled={ !isValid }>Submit</button>
+            { !isValid && 
+                <div className="alert alert-primary" role='alert'>
+                    Please complete the above fields.
+                </div>
+            }
         </form>
     );
 }
